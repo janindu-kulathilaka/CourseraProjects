531ba8cd3e5ce4b98067e4e8fd4890acddded373
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-06-03 11:39:51 +0530

    2023-6-3

--- React_Basics/week2/weektwo/src/App.js
+++ React_Basics/week2/weektwo/src/App.js
@@ -1,33 +1,49 @@
-function Main(props) {
-  return <Header msg={props.msg} />;
-}
+//relationship between child and parent
 
-function Header(props) {
-  return (
-    <div style={{ border: "10px solid whitesmoke" }}>
-      <h1>Header here</h1>
-      <Wrapper msg={props.msg} />
-    </div>
-  );
-}
+// function Main(props) {
+//   return <Header msg={props.msg} />;
+// }
 
-function Wrapper(props) {
-  return (
-    <div style={{ border: "10px solid lightgray" }}>
-      <h2>Wrapper here</h2>
-      <Button msg={props.msg} />
-    </div>
-  );
-}
+// function Header(props) {
+//   return (
+//     <div style={{ border: "10px solid whitesmoke" }}>
+//       <h1>Header here</h1>
+//       <Wrapper msg={props.msg} />
+//     </div>
+//   );
+// }
 
-function Button(props) {
+// function Wrapper(props) {
+//   return (
+//     <div style={{ border: "10px solid lightgray" }}>
+//       <h2>Wrapper here</h2>
+//       <Button msg={props.msg} />
+//     </div>
+//   );
+// }
+
+// function Button(props) {
+//   return (
+//     <div style={{ border: "20px solid orange" }}>
+//       <h3>This is the button component</h3>
+//       <button onClick={() => alert(props.msg)}>Click me!</button>
+//     </div>
+//   );
+// }
+
+import React from "react";
+
+const MealsContext = React.createContext();
+
+const todaysMeals = ["Baked Beans", "Baked Sweet Potatoes", "Baked Potatoes"];
+
+const MealsProvider = ({ children }) => {
+  const [meals, setMealsList] = React.useState(todaysMeals);
+
   return (
-    <div style={{ border: "20px solid orange" }}>
-      <h3>This is the button component</h3>
-      <button onClick={() => alert(props.msg)}>Click me!</button>
-    </div>
+    <MealsContext.Provider value={{ meals }}>{children}</MealsContext.Provider>
   );
-}
+};
 
 function App() {
   return (
---
+++ React_Basics/week2/weektwo/src/MealsProvider.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MealsContext = React.createContext();
+
+const todaysMeals = ["Baked Beans", "Baked Sweet Potatoes", "Baked Potatoes"];
+
+const MealsProvider = ({ children }) => {
+  const [meals, setMealsList] = React.useState(todaysMeals);
+
+  return (
+    <MealsContext.Provider value={{ meals }}>{children}</MealsContext.Provider>
+  );
+};
+
+export const userMealsListContext = () => React.useContext(MealsContext);
+
+export default MealsProvider;
