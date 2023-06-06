4d04c3f6debd61360ff073a6c71904d4eadcf1b3
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-06-07 05:25:32 +0530

    almost done third week

--- React_Basics/week2lab1/src/App.js
+++ React_Basics/week2lab1/src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import Homepage from "./Homepage";
 import AboutMe from "./AboutMe";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [fruits] = React.useState([
-    { fruitName: "apple", id: 1 },
-    { fruitName: "apple", id: 2 },
-    { fruitName: "plum", id: 3 },
-  ]);
+  // const [fruits] = React.useState([
+  //   { fruitName: "apple", id: 1 },
+  //   { fruitName: "apple", id: 2 },
+  //   { fruitName: "plum", id: 3 },
+  // ]);
   return (
     <div className="App">
       <nav className="nav">
