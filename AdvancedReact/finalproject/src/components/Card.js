bb9fbdfae3f1a1aa6aea370c221fb8a32c5f3cd3
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-07-05 11:23:43 +0530

    doing the final project of the advanced react course

--- AdvancedReact/finalproject/src/components/Card.js
+++ AdvancedReact/finalproject/src/components/Card.js
@@ -1,13 +1,39 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack
+      spacing={4}
+      align="start"
+      p={4}
+      borderWidth={1}
+      borderRadius="md"
+      boxShadow="md"
+      width="300px"
+    >
+      <Box width="100%">
+        <Image
+          src={imageSrc}
+          alt={title}
+          objectFit="cover"
+          width="100%"
+          height="100%"
+        />
+      </Box>
+
+      <VStack align="start" spacing={2} flex={1}>
+        <Heading size="md">{title}</Heading>
+        <Text>{description}</Text>
+      </VStack>
+
+      <Box alignSelf="flex-end">
+        <FontAwesomeIcon icon={faArrowRight} />
+      </Box>
+    </VStack>
+  );
 };
 
 export default Card;
--- AdvancedReact/finalproject/src/components/LandingSection.js
+++ AdvancedReact/finalproject/src/components/LandingSection.js
@@ -6,15 +6,30 @@ const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
     alignItems="center"
     isDarkBackground
     backgroundColor="#2A4365"
-  ></FullScreenSection>
+  >
+    <VStack spacing={4} alignItems="center">
+      <Avatar size="xl" name="Pete" src="/avatar.png" />
+
+      {/* Use smaller size for the greeting */}
+      <Heading as="h1" size="md" color="white">
+        {greeting}
+      </Heading>
+
+      {/* Use larger sizes for bio1 and bio2 */}
+      <Heading as="h2" size="xl" color="white">
+        {bio1}
+      </Heading>
+      <Heading as="h2" size="xl" color="white">
+        {bio2}
+      </Heading>
+    </VStack>
+  </FullScreenSection>
 );
 
 export default LandingSection;
